40f2b3d3505ef48427da512aa466a2ec6e12846e
atbys <ak.tamalin@gmail.com>
2020-11-03 18:18:46 +0900

    change template path

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,8 +10,9 @@ var engine *gin.Engine
 
 func main() {
 	engine = gin.Default()
-	engine.LoadHTMLGlob("./template/*")
-	initializeSession()
+	gopath := os.Getenv("GOPATH")
+	engine.LoadHTMLGlob(gopath + "/src/github.com/atbys/todays/template/*")
+	initializeSession() //セッション管理の初期化の関係で絶対にこっちを先に処理する
 	initializeRoutes()
 
 	engine.Run(":8080")
