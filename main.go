a540e6a4c7060bc42ea0821173ffa355fae49fec
atbys <ak.tamalin@gmail.com>
2020-11-03 13:59:20 +0900

    show html page

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
+	engine.LoadHTMLGlob("./template/*")
 	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
+		render(c, gin.H{
+			"description": 1907,
+		}, "index.html")
 	})
 	engine.Run(":8080")
 }
